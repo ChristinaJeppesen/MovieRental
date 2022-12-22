f4533046016b4a2d9fc2ed80be2278d40f69d93c
Christina Jeppesen <s174242@student.dtu.dk>
2022-12-22 22:50:42 +0100

    final push

---
+++ Demo/SharedModelLibrary/SharedModelLibrary/CustomerMovieList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SharedModelLibrary
+{
+    public class CustomerMovieList
+    {
+        public Guid CustomerId { get; set; }
+
+        public List<int> MovieIdList { get; set; }
+
+        [JsonConstructor]
+        public CustomerMovieList()
+        {
+        }
+
+        public CustomerMovieList(Guid customerId, List<int> movieIdList)
+        {
+            CustomerId = customerId;
+            MovieIdList = movieIdList;
+        }
+    }
+}
